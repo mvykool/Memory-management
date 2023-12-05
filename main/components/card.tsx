16a38f2759346472517a856ac18299c5cef3ff7e
mvykool <maykolandres4@gmail.com>
2023-12-05 12:00:00 -0500

    changed scale

--- main/components/card.tsx
+++ main/components/card.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function card(): JSX.Element {
   return (
-    <div className="relative transform transition duration-300 hover:scale-110 rounded-lg shadow-lg h-52 w-56 hover:shadow-xl bg-white">
+    <div className="relative transform transition duration-300 hover:scale-105 rounded-lg shadow-lg h-52 w-56 hover:shadow-xl bg-white">
       <div className="bg-gradient-to-br from-rose-100 via-purple-200 to-purple-200 m-2 h-3/6 rounded-lg"></div>
 
       <div className="px-5 pt-2 flex flex-col">
-        <h2 className="font-semibold text-black">Card</h2>
+        <h2 className="font-semibold text-black">Title</h2>
 
         <button
           className="bg-blue-500 cursor-pointer text-white px-2 py-1 mt-2 rounded-md transition duration-150 hover:bg-blue-700"
