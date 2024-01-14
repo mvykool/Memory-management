20f3b8de0b5182e2c3448843a7329c3b79a90bc0
mvykool <maykolandres4@gmail.com>
2024-01-14 15:45:58 -0500

    added bg

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -14,8 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "space-black": "#06050F"
-      }
+        "space-black": "#06050F",
+      },
     },
   },
   plugins: [],
