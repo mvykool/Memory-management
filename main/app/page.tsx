91a9050d89f002fa8bd800feb501764a75d6e71b
mvykool <maykolandres4@gmail.com>
2024-01-14 13:35:43 -0500

    implemented title and more

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,24 +1,36 @@
-import { Roboto, Lato } from 'next/font/google'
+import { Roboto, Lato } from "next/font/google";
 
 const roboto = Roboto({
-  weight: '900',
-  subsets: ['latin'],
-})
+  weight: "900",
+  subsets: ["latin"],
+});
 
 const lato = Lato({
-  weight: '400',
-  subsets: ['latin'],
-})
-
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home(): JSX.Element {
   return (
     <div className="flex min-h-screen">
       <div className="w-3/6 my-52">
-        <h1 className={roboto.className + " text-6xl my-3 font-semibold"}>Production ready components</h1>
-        <p className={lato.className + " w-4/6 text-lg tracking-wide mt-7 opacity-85"} > Beautifully designed, expertly crafted components and templates, built by the makers of Tailwind CSS. The perfect starting point for your next project.</p>
-        <button className='my-7 bg-pink-700 px-5 py-2 rounded-sm'>Browse components</button>
+        <h1 className={roboto.className + " text-6xl my-3 font-semibold"}>
+          Production ready components
+        </h1>
+        <p
+          className={
+            lato.className + " w-4/6 text-lg tracking-wide mt-7 opacity-85"
+          }
+        >
+          {" "}
+          Beautifully designed, expertly crafted components and templates, built
+          by the makers of Tailwind CSS. The perfect starting point for your
+          next project.
+        </p>
+        <button className="my-7 bg-pink-700 px-5 py-2 rounded-sm">
+          Browse components
+        </button>
       </div>
-    </div >
+    </div>
   );
 }
