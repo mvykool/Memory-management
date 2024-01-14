959e69b8387e3f6b9b59ea7c9e406192a72cc77a
mvykool <maykolandres4@gmail.com>
2024-01-13 19:52:48 -0500

    added navbar

--- main/app/layout.tsx
+++ main/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../style/globals.css";
+import Navbar from "../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,13 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ main/components/navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const Navbar = (): JSX.Element => {
+  return (
+    <nav className='text-white'> <p>hola</p></ nav>
+  )
+}
+
+export default Navbar
