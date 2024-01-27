a48d829587e2641b8ae6a42bd8cc6c7bdb481cd2
mvykool <maykolandres4@gmail.com>
2024-01-27 12:00:00 -0500

    changed gradient

--- main/components/landingButton.tsx
+++ main/components/landingButton.tsx
@@ -7,7 +7,7 @@ export default function LandingButton(): JSX.Element {
         <button className="w-28 h-12 text-white font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:cursor-pointer">
           Click me!
         </button>
-        <button className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-2 px-4 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
+        <button className="bg-gradient-to-r from-pink-500 to-purple-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-2 px-4 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
           Keep holding
         </button>
       </div>
