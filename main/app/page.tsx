62321b27afa3b67fedf20147e84689ae8527f054
mvykool <maykolandres4@gmail.com>
2024-01-31 22:46:19 -0500

    added card

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from "next/font/google";
 import Button from "../components/button";
+import Card from "../components/card";
 const roboto = Roboto({
   weight: "900",
   subsets: ["latin"],
@@ -11,10 +12,15 @@ export default function Home(): JSX.Element {
   return (
     <div className="flex min-h-screen">
       <div className="bg-img blur-3xl"></div>
-      <div className="w-3/6 my-52 z-10">
-        <h1 className={roboto.className + " text-6xl my-3 font-semibold"}>
-          Production ready components
-        </h1>
+      <div className="w-5/6 my-52 z-10">
+        <div className="flex justify-between w-full">
+          <h1
+            className={roboto.className + " text-6xl w-3/6 my-3 font-semibold"}
+          >
+            Production ready components
+          </h1>
+          <Card />
+        </div>
         <p className="w-4/6 text-lg tracking-wide mt-7 opacity-85">
           {" "}
           Beautifully designed, expertly crafted components and templates, built
