50a1c09d4bdcc2bd7d681b15b2956fc13ec52cae
mvykool <maykolandres4@gmail.com>
2024-02-03 12:00:00 -0500

    removed gradient

--- main/components/landingButton.tsx
+++ main/components/landingButton.tsx
@@ -4,7 +4,7 @@ export default function LandingButton(): JSX.Element {
   return (
     <>
       <div className="flex gap-3">
-        <button className="w-28 h-12 text-white font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:cursor-pointer">
+        <button className="w-28 h-12 text-white font-semibold bg-teal-500 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:cursor-pointer">
           Click me!
         </button>
         <button className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-500 hover:to-purple-600 text-white font-bold py-2 px-4 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
