01ec1c05991957780ea7d0d4b553bc8d13410e55
mvykool <maykolandres4@gmail.com>
2024-02-05 12:00:00 -0500

    added hover bg

--- main/components/landingButton.tsx
+++ main/components/landingButton.tsx
@@ -4,7 +4,7 @@ export default function LandingButton(): JSX.Element {
   return (
     <>
       <div className="flex gap-3">
-        <button className="w-28 h-12 text-white font-semibold bg-teal-500 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:cursor-pointer">
+        <button className="w-28 h-12 text-white font-semibold bg-teal-500 rounded-lg shadow-lg hover:scale-105 hover:bg-teal-700 duration-200 hover:drop-shadow-2xl hover:cursor-pointer">
           Click me!
         </button>
         <button className="bg-violet-500 hover:bg-violet-700 font-bold py-2 px-4 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
