c1fee7cb034ad1dc28e63cec8893d6ddb24c033f
mvykool <maykolandres4@gmail.com>
2024-03-02 21:57:21 -0500

    modified some colors and button placement

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -15,16 +15,14 @@ export default function Home(): JSX.Element {
       <div className="w-5/6 my-52 z-10">
         <div className="flex justify-between w-full">
           <div className="flex gap-4 flex-col w-3/6">
-            <h1
-              className={roboto.className + " text-6xl font-semibold"}
-            >
+            <h1 className={roboto.className + " text-6xl font-semibold"}>
               Production ready components
             </h1>
             <p className="w-5/6 text-lg tracking-wide opacity-85">
               {" "}
-              Beautifully designed, expertly crafted components and templates, built
-              by the makers of Tailwind CSS. The perfect starting point for your
-              next project.
+              Beautifully designed, expertly crafted components and templates,
+              built by the makers of Tailwind CSS. The perfect starting point
+              for your next project.
             </p>
             <Button buttonText={text}></Button>
           </div>
