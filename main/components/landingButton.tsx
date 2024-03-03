a18b5a227a51e52f0e2e9c294d0daa3bf13edd09
mvykool <maykolandres4@gmail.com>
2024-03-02 22:27:58 -0500

    moved and created button

--- main/components/landingButton.tsx
+++ main/components/landingButton.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function LandingButton(): JSX.Element {
-  return (
-    <button>Click me!</button>
-  )
+  return <button>Click me!</button>;
 }
-
