8a7c8c70403983cb089778a2a1be2ec8fa8099f5
mvykool <maykolandres4@gmail.com>
2024-03-29 22:48:37 -0500

    added path

--- main/components/button.tsx
+++ main/components/button.tsx
@@ -6,7 +6,7 @@ interface CustomButtonProps {
 
 const Button = ({ buttonText }: CustomButtonProps): JSX.Element => {
   return (
-    <button className="my-7 bg-neon-purple px-5 py-2 rounded-sm w-[15rem] text-center">
+    <button className="my-7 bg-neon-purple px-5 py-2 rounded-sm w-[15rem] text-center hover:bg-pink-700">
       {buttonText}
     </button>
   );
